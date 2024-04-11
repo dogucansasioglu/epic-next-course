6e5cb9c20a30139f798ad2398679794f4fc656ac
Doğucan Şaşıoğlu <sasiogludogucan@gmail.com>
2024-04-11 07:29:09 +0300

    init repo

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,23 @@
+const getStrapiData = async (path: string) => {
+	const baseUrl = "http://localhost:1337";
+	try {
+		const response = await fetch(baseUrl + path);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export default async function Home() {
+	const strapiData = await getStrapiData("/api/home-page");
+
+	const { title, description } = strapiData.data.attributes;
+
+	return (
+		<div className="container mx-auto py-6">
+			<h1 className="text-5xl font-bold">{title}</h1>
+			{/* <p className="text-xl mt-4">{description}</p> */}
+		</div>
+	);
+}
