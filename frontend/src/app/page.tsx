b1f3714232f3af0be8a723dcb1c96ff9cfa0331b
Doğucan Şaşıoğlu <sasiogludogucan@gmail.com>
2024-04-11 07:31:45 +0300

    test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
 	return (
 		<div className="container mx-auto py-6">
 			<h1 className="text-5xl font-bold">{title}</h1>
-			{/* <p className="text-xl mt-4">{description}</p> */}
+			<p className="text-xl mt-4">{description}</p>
 		</div>
 	);
 }
